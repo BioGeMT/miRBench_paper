a176bebf1de6ecf13a5e0febb551fc440ba9df66
Stephanie <stephanie.sammut@um.edu.mt>
2024-08-07 09:03:27 +0000

    using download_dataset

--- code/benchmark_all/benchmark_all.py
+++ code/benchmark_all/benchmark_all.py
@@ -18,7 +18,6 @@ def yield_blocks(file_path, block_size):
         if block:
             yield pd.DataFrame(block, columns=header)
 
-
 def benchmark_all(block, dset, ratio):
     for tool in miRBench.predictor.list_predictors():
         print(f"Running {tool} on {dset} dataset, ratio {ratio}")           
@@ -32,19 +31,21 @@ def benchmark_all(block, dset, ratio):
 
 def main():
     parser = argparse.ArgumentParser(description="Benchmark all available predictors on all available datasets")
-    parser.add_argument("--out_dir", type=str, default=".", help="Output directory for predictions")
+    parser.add_argument("--out_dir", type=str, default=".", help="Output directory for predictions")\
+    parser.add_argument("--download_dir", type=str, default=".", help="Directory to download datasets to")
     args = parser.parse_args()
 
     # loop over all available datasets
     for dset in miRBench.dataset.list_datasets():
         for ratio in ["1", "10", "100"]:
             print(f"Downloading {dset} dataset, ratio {ratio}")
-            df = miRBench.dataset.get_dataset(dset, split="test", ratio=ratio)
+            download_dir = args.download_dir
+            dset_path = miRBench.dataset.download_dataset(download_dir, split="test", ratio=ratio)
             output_file = os.path.join(args.out_dir, f"{dset}_{ratio}_predictions.tsv")
             header_written = False
-            for block in yield_blocks(dset, 339066): 
+            for block in yield_blocks(dset_df, 339066): 
                 # 339066 is the number of examples in AGO2_eCLIP_Manakov2022_1_test_dataset.tsv, to be processed in one batch.
-                # AGO2_eCLIP_Manakov2022_10_test_dataset.tsv and AGO2_eCLIP_Manakov2022_100_test_dataset.tsv will be processed in batches.
+                # AGO2_eCLIP_Manakov2022_10_test_dataset.tsv and AGO2_eCLIP_Manakov2022_100_test_dataset.tsv will be processed in multiple batches.
                 block_preds = benchmark_all(block, dset, ratio)
                 if not header_written:
                     block_preds.to_csv(output_file, sep='\t', index=False, mode='w')
--- code/benchmark_all/run_benchmark_all.sh
+++ code/benchmark_all/run_benchmark_all.sh
@@ -6,11 +6,13 @@
 
 # Initialize variables
 OUT_DIR=""
+DWNLD_DIR=""
 
 # Parse command-line options
-while getopts ":o:" opt; do
+while getopts ":o:d:" opt; do
   case $opt in
     o) OUT_DIR="$OPTARG" ;;
+    d) DWNLD_DIR="$OPTARG" ;;
     \?) echo "Invalid option: -$OPTARG" >&2
       exit 1 ;;
     :) echo "Option -$OPTARG requires an argument." >&2
@@ -20,10 +22,17 @@ done
 
 # Check if OUT_DIR is provided
 if [ -z "$OUT_DIR" ]; then
-    echo "Error: Output directory not specified."
+    echo "Error: Output directory not specified. This is where the results will be saved."
     echo "Usage: sbatch run_benchmark_all.sh -o <output_directory>"
     exit 1
 fi
 
-python benchmark_all.py --out_dir $OUT_DIR
+# Check if DWNLD_DIR is provided
+if [ -z "$DWNLD_DIR" ]; then
+    echo "Error: Download directory not specified. This is where the datasets will be downloaded."
+    echo "Usage: sbatch run_benchmark_all.sh -o <output_directory> -d <download_directory>"
+    exit 1
+fi
+
+python benchmark_all.py --out_dir $OUT_DIR --download_dir $DWNLD_DIR
 
