08596bb94f21bbdae2c8d9ea6ace3d743bb861c5
Stephanie <stephanie.sammut@um.edu.mt>
2024-08-19 10:07:50 +0000

    updated pr_curves.py

--- code/pr_curves/pr_curves.py
+++ code/pr_curves/pr_curves.py
@@ -16,6 +16,9 @@ def plot_pr_curve(data, predictors, figsize=(6, 6), dpi=300, title=None):
     colors = plt.cm.tab20(np.linspace(0, 1, 11))
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
 
+    markers = ['o', 's', '^', 'X']
+    i = 0
+
     fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
 
     for predictor in predictors:
@@ -25,7 +28,8 @@ def plot_pr_curve(data, predictors, figsize=(6, 6), dpi=300, title=None):
             
         if predictor.startswith('Seed'):
             p, r, _, _ = precision_recall_fscore_support(data['label'].values, data[predictor].values, average='binary')
-            ax.plot(r, p, 'o', label=predictor)
+            ax.plot(r, p, color='black', marker=markers[i], label=predictor)
+            i += 1
         else:
             precision, recall, _ = precision_recall_curve(data['label'], data[predictor])
             ax.plot(recall, precision, label=predictor)
@@ -64,12 +68,22 @@ def main():
 
     # if predictors is none, set it to all columns, except columns noncodingRNA, gene and label
     if args.predictors is None:
-        args.predictors = [col for col in data.columns if col not in ['gene', 'noncodingRNA', 'noncodingRNA_fam', 'feature', 'label']]
+        args.predictors = ['TargetScanCnn_McGeary2019',
+            'CnnMirTarget_Zheng2020',
+            'TargetNet_Min2021',
+            'miRBind_Klimentova2022',
+            'miRNA_CNN_Hejret2023',
+            'InteractionAwareModel_Yang2024',
+            'RNACofold',
+            'Seed8mer', 
+            'Seed7mer',
+            'Seed6mer', 
+            'Seed6merBulgeorMismatch'] # in chronological order
 
     # plot precision-recall curves for all predictors
     fig, ax = plot_pr_curve(data, args.predictors, title=args.title)
 
-    plt.savefig(args.ofile)
+    plt.savefig(args.ofile, format='svg')
     plt.close()
 
 if __name__ == "__main__":
