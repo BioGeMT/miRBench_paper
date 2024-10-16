2b639a1695eb343ee7e104419d330a14806612de
Stephanie <stephanie.sammut@um.edu.mt>
2024-10-16 15:55:05 +0000

    merged add_cons and validate_cons into a single script

--- code/conservation/add_conservation_scores.py
+++ code/conservation/add_conservation_scores.py
@@ -1,7 +1,28 @@
 import pandas as pd
+import numpy as np
 import pyBigWig
 import argparse
 import warnings 
+
+def read_input(input_file):
+    # Read input data from a file or stdin.
+    try:
+        if input_file:
+            data = pd.read_csv(input_file, sep='\t', dtype={'chr': str})
+        else:
+            data = pd.read_csv(sys.stdin, sep='\t', low_memory=False)
+    except Exception as e:
+        warnings.warn(f"Error reading input data: {e}", category=UserWarning)
+        sys.exit(1)
+    return data
+
+# Check if a value can be converted to a float i.e. is a valid number
+def is_valid_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 def get_conservation(bw_file, chrom, start, end, chrom_sizes):
     # Convert to string
@@ -16,21 +37,37 @@ def get_conservation(bw_file, chrom, start, end, chrom_sizes):
     # Check if chromosome exists in the BigWig file
     if chrom not in chrom_sizes:
         print(f"Chromosome {chrom} not found in BigWig file")
-        return [float('nan')] * (int(end) - int(start))
+        return np.nan
 
     try:
         start = int(float(start)) - 1  # Convert to 0-based
         end = int(float(end))
         chrom_size = chrom_sizes[chrom]
-        if start >= chrom_size or end <= 0 or start >= end:
+        # Check if interval is valid
+        if start < 0 or start >= chrom_size or end <= 0 or end > chrom_size or start >= end:
             print(f"Invalid interval for {chrom}:{start}-{end}")
-            return [float('nan')] * (end - start)
+            return np.nan 
+        # Get conservation score values
         start = max(0, start)
         end = min(chrom_size, end)
-        return bw_file.values(chrom, start, end)
+        conservation_scores = bw_file.values(chrom, start, end)
+        # Check if all values are valid numbers
+        if all(is_valid_number(score) for score in conservation_scores):
+            return conservation_scores
+        else:
+            print(f"Invalid conservation score numbers for {chrom}:{start}-{end}")
+            return np.nan
     except Exception as e:
         print(f"Error processing {chrom}:{start}-{end}: {e}")
-        return [float('nan')] * (end - start)
+        return np.nan
+
+def check_conservation_length(row, column):
+    try:
+        conservation_scores_length = len(row[column])
+        target_length = len(row['gene'])
+        return conservation_scores_length == target_length
+    except:
+        return False
 
 def add_conservation(df, phyloP_path, phastCons_path, ofile):
     # Open BigWig files
@@ -40,7 +77,6 @@ def add_conservation(df, phyloP_path, phastCons_path, ofile):
         chrom_sizes_phyloP = bw_phyloP.chroms()
         chrom_sizes_phastCons = bw_phastCons.chroms()
 
-        header = True
         columns = ['chr', 'start', 'end', 'strand']
 
         with open(ofile, 'a') as ofile:                
@@ -48,25 +84,13 @@ def add_conservation(df, phyloP_path, phastCons_path, ofile):
             df['gene_phyloP'] = df.apply(lambda row: get_conservation(bw_phyloP, row['chr'], row['start'], row['end'], chrom_sizes_phyloP), axis=1)
             df['gene_phastCons'] = df.apply(lambda row: get_conservation(bw_phastCons, row['chr'], row['start'], row['end'], chrom_sizes_phastCons), axis=1)
 
-            if header:
-                df.head(0).to_csv(ofile, sep='\t', index=False, mode='w')
-                header = False
+            # Check if conservation scores have the same length as the gene sequence, otherwise set to NaN
+            df['gene_phyloP'] = df.apply(lambda row: row['gene_phyloP'] if check_conservation_length(row, 'gene_phyloP') else np.nan, axis=1)
+            df['gene_phastCons'] = df.apply(lambda row: row['gene_phastCons'] if check_conservation_length(row, 'gene_phastCons') else np.nan, axis=1)
                     
-            df.to_csv(ofile, sep='\t', index=False, header=False, mode='a')
-    
+            df.to_csv(ofile, sep='\t', index=False, header=True, mode='a')
 
-def read_input(input_file):
-    # Read input data from a file or stdin.
-    try:
-        if input_file:
-            data = pd.read_csv(input_file, sep='\t', dtype={'chr': str})
-        else:
-            data = pd.read_csv(sys.stdin, sep='\t', low_memory=False)
-    except Exception as e:
-        warnings.warn(f"Error reading input data: {e}", category=UserWarning)
-        sys.exit(1)
-    return data
-
+        return df
 
 def main():
     parser = argparse.ArgumentParser(description="Add conservation scores.")
