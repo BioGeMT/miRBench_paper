135c93dbc5851a6d0646223190842973368c264c
evaklimentova <klimentova.eva@seznam.cz>
2024-10-16 11:11:18 +0200

    cleanup of training and encoding

--- code/model_training/encode_dataset.py
+++ code/model_training/encode_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import argparse
 import time
-    
+
 
 def binding_encoding(df, alphabet, tensor_dim=(50, 20, 1)):
     """
@@ -20,7 +20,7 @@ def binding_encoding(df, alphabet, tensor_dim=(50, 20, 1)):
 
     # Initialize dot matrix with zeros
     ohe_matrix_2d = np.zeros((len(df), *tensor_dim), dtype="float32")
-    
+
     df = df.reset_index(drop=True)
 
     # Compile matrix with Watson-Crick interactions
@@ -34,12 +34,13 @@ def binding_encoding(df, alphabet, tensor_dim=(50, 20, 1)):
 
     return ohe_matrix_2d, labels
 
-def encode_large_csv_to_numpy(csv_file_path, data_output_path, labels_output_path, chunk_size=10000):
+
+def encode_large_tsv_to_numpy(tsv_file_path, data_output_path, labels_output_path, chunk_size=10000):
     """
-    Encode a large CSV file into a NumPy matrix using chunk processing.
+    Encode a large TSV file into a NumPy matrix using chunk processing.
 
     Parameters:
-    - csv_file_path: Path to the CSV file.
+    - tsv_file_path: Path to the TSV file with dataset.
     - data_output_path: Path to the output data .npy file.
     - labels_output_path: Path to the output labels .npy file.
     - chunk_size: Number of rows to process at a time.
@@ -49,8 +50,8 @@ def encode_large_csv_to_numpy(csv_file_path, data_output_path, labels_output_pat
     tensor_dim = (50, 20, 1)
 
     # Get total number of rows in the dataset
-    num_rows = sum(len(df) for df in pd.read_csv(csv_file_path, sep='\t', usecols=[0], chunksize=chunk_size))
-    
+    num_rows = sum(len(df) for df in pd.read_csv(tsv_file_path, sep='\t', usecols=[0], chunksize=chunk_size))
+
     # Determine the shape of the output arrays
     labels_shape = (num_rows,)
     data_shape = (num_rows, *tensor_dim)
@@ -62,9 +63,9 @@ def encode_large_csv_to_numpy(csv_file_path, data_output_path, labels_output_pat
     row_offset = 0
 
     # Process each chunk
-    for chunk in pd.read_csv(csv_file_path, sep='\t', chunksize=chunk_size):
+    for chunk in pd.read_csv(tsv_file_path, sep='\t', chunksize=chunk_size):
         encoded_data, encoded_labels = binding_encoding(chunk, alphabet, tensor_dim)
-        
+
         # Write the chunk's data and labels to the memory-mapped files
         ohe_matrix_2d[row_offset:row_offset + len(chunk)] = encoded_data
         labels[row_offset:row_offset + len(chunk)] = encoded_labels
@@ -74,17 +75,28 @@ def encode_large_csv_to_numpy(csv_file_path, data_output_path, labels_output_pat
     ohe_matrix_2d.flush()
     labels.flush()
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Encode dataset to miRNA x target binding matrix")
-    parser.add_argument('--ifile', type=str, required=True, help="Input dataset file name")
-    parser.add_argument('--ofile', type=str, required=True, help="Output file name prefix")
+    """
+    Based on "miRBind: A deep learning method for miRNA binding classification." Genes 13.12 (2022): 2323. https://doi.org/10.3390/genes13122323.
+    Original implementation: https://github.com/ML-Bioinfo-CEITEC/miRBind
+
+    Encodes miRNA and gene sequences into 2D-binding matrix.
+    2D-binding matrix has shape (gene_max_len=50, miRNA_max_len=20, 1) and contains 1 for Watson-Crick interactions and 0 otherwise.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Encode dataset to miRNA x target binding matrix. Outputs numpy file with matrices and and numpy file with corresponding labels. Expected columns of the dataset are 'noncodingRNA', 'gene' and 'label'")
+    parser.add_argument('-i', '--i_file', type=str, required=True, help="Input dataset file name")
+    parser.add_argument('-o', '--o_prefix', type=str, required=True, help="Output file name prefix")
     args = parser.parse_args()
-    
+
     start = time.time()
-    encode_large_csv_to_numpy(args.ifile, args.ofile + '_dataset.npy', args.ofile + '_labels.npy')
+    encode_large_tsv_to_numpy(args.i_file, args.o_prefix + '_dataset.npy', args.o_prefix + '_labels.npy')
     end = time.time()
-    
+
     print("Elapsed time: ", end - start, " s.")
+
 
 if __name__ == "__main__":
     main()
