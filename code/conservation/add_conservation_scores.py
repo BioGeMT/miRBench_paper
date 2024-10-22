4366a3222873783541cc7125ca0deebf62185b72
Stephanie <stephanie.sammut@um.edu.mt>
2024-10-22 13:54:27 +0000

    refactored to validate scores prior to adding, otherwise adding np.nan

--- code/conservation/add_conservation_scores.py
+++ code/conservation/add_conservation_scores.py
@@ -11,21 +11,26 @@ def read_input(input_file):
             data = pd.read_csv(input_file, sep='\t', dtype={'chr': str})
         else:
             data = pd.read_csv(sys.stdin, sep='\t', low_memory=False)
+    
     except Exception as e:
         warnings.warn(f"Error reading input data: {e}", category=UserWarning)
         sys.exit(1)
+    
     return data
 
-# Check if a value can be converted to a float i.e. is a valid number
+# Check if a value can be converted to a float i.e. is a number, and if a number is not NaN
 def is_valid_number(s):
     try:
-        float(s)
+        num = float(s)
+        if np.isnan(num):
+            return False
         return True
+
     except ValueError:
         return False
 
-def get_conservation(bw_file, chrom, start, end, chrom_sizes):
-    # Convert to string
+def get_valid_conservation(bw_file, chrom, start, end, chrom_sizes, gene):
+    # Convert chrom to string
     chrom = str(chrom)
     
     # Special handling for mitochondrial chromosome
@@ -36,41 +41,43 @@ def get_conservation(bw_file, chrom, start, end, chrom_sizes):
     
     # Check if chromosome exists in the BigWig file
     if chrom not in chrom_sizes:
-        print(f"Chromosome {chrom} not found in BigWig file")
+        print(f"Chromosome {chrom} not found in BigWig file. Returning NaN.")
         return np.nan
 
     try:
-        start = int(float(start)) - 1  # Convert to 0-based
+        # Convert start and end to integer, and adjust start to 0-based
+        start = int(float(start)) - 1 
         end = int(float(end))
+
+        # Get chromosome size
         chrom_size = chrom_sizes[chrom]
+
         # Check if interval is valid
         if start < 0 or start >= chrom_size or end <= 0 or end > chrom_size or start >= end:
-            print(f"Invalid interval for {chrom}:{start}-{end}")
+            print(f"Invalid interval for {chrom}:{start}-{end}. Returning NaN.")
             return np.nan 
+
         # Get conservation score values
         conservation_scores = bw_file.values(chrom, start, end)
-        # Check if all values are valid numbers
-        if all(is_valid_number(score) for score in conservation_scores):
-            return conservation_scores
-        else:
-            print(f"Invalid conservation score numbers for {chrom}:{start}-{end}")
-            return np.nan
-    except Exception as e:
-        print(f"Error processing {chrom}:{start}-{end}: {e}")
-        return np.nan
 
-def check_conservation_length(row, column):
-    try:
-        conservation_scores_length = len(row[column])
-        target_length = len(row['gene'])
+        # Check if all values are valid numbers
+        if not all(is_valid_number(score) for score in conservation_scores):
+            print(f"Invalid conservation score values for {chrom}:{start}-{end}. Returning NaN.")
+            return np.nan
+
+        # Check if length of conservation scores matches gene sequence length
+        conservation_scores_length = len(conservation_scores)
+        target_length = len(gene)
         if conservation_scores_length != target_length:
-            print(f"Conservation scores length does not match gene sequence length for {row['chr']}:{row['start']}-{row['end']}")
-            return False
-        else:
-            return True
-    except:
-        print(f"Error checking conservation scores length for {row['chr']}:{row['start']}-{row['end']}")
-        return False
+            print(f"Conservation scores length does not match gene sequence length for {chrom}:{start}-{end}. Returning NaN.")
+            return np.nan
+
+        # Return conservation scores if all checks pass    
+        return conservation_scores
+
+    except Exception as e:
+        print(f"Error processing {chrom}:{start}-{end}: {e}. Returning NaN.")
+        return np.nan
 
 def add_conservation(df, phyloP_path, phastCons_path, ofile):
     # Open BigWig files
@@ -80,16 +87,10 @@ def add_conservation(df, phyloP_path, phastCons_path, ofile):
         chrom_sizes_phyloP = bw_phyloP.chroms()
         chrom_sizes_phastCons = bw_phastCons.chroms()
 
-        columns = ['chr', 'start', 'end', 'strand']
-
         with open(ofile, 'a') as ofile:                
             # Add conservation scores
-            df['gene_phyloP'] = df.apply(lambda row: get_conservation(bw_phyloP, row['chr'], row['start'], row['end'], chrom_sizes_phyloP), axis=1)
-            df['gene_phastCons'] = df.apply(lambda row: get_conservation(bw_phastCons, row['chr'], row['start'], row['end'], chrom_sizes_phastCons), axis=1)
-
-            # Check if conservation scores have the same length as the gene sequence, otherwise set to NaN
-            df['gene_phyloP'] = df.apply(lambda row: row['gene_phyloP'] if check_conservation_length(row, 'gene_phyloP') else np.nan, axis=1)
-            df['gene_phastCons'] = df.apply(lambda row: row['gene_phastCons'] if check_conservation_length(row, 'gene_phastCons') else np.nan, axis=1)
+            df['gene_phyloP'] = df.apply(lambda row: get_valid_conservation(bw_phyloP, row['chr'], row['start'], row['end'], chrom_sizes_phyloP, row['gene']), axis=1)
+            df['gene_phastCons'] = df.apply(lambda row: get_valid_conservation(bw_phastCons, row['chr'], row['start'], row['end'], chrom_sizes_phastCons, row['gene']), axis=1)
                     
             df.to_csv(ofile, sep='\t', index=False, header=True, mode='a')
 
