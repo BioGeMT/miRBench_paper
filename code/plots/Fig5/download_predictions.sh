af09694ec68dfaeb89b1e67face0e65767e11912
David Cechak <davidcechak@gmail.com>
2024-11-13 15:32:05 +0100

    Download only the testing dataset used

--- code/plots/Fig5/download_predictions.sh
+++ code/plots/Fig5/download_predictions.sh
@@ -7,22 +7,13 @@ then
     pip install gdown
 fi
 
-# Define file IDs for the specific .tsv files you want to download
+download_dir="predictions"
 FILE_ID_1="15gQ4WShxUZjEjSwppc9cukfusanpbl6n"
-FILE_ID_2="1fWD_5TVu1B6RWJ2aVm69M2TPbJ8Gh75Z"
-FILE_ID_3="1_GYAxu5DzudQ5ka-zwqEa4friyMhF5vn"
 
 # Create a directory to store the downloaded .tsv files
-mkdir -p predictions
+mkdir -p $download_dir
 
-# Download each file by its ID
 echo "Downloading AGO2_eCLIP_Manakov2022_100_CNN_predictions.tsv"
-gdown "https://drive.google.com/uc?id=$FILE_ID_1" -O predictions/AGO2_eCLIP_Manakov2022_100_CNN_predictions.tsv
+gdown "https://drive.google.com/uc?id=$FILE_ID_1" -O $download_dir/AGO2_eCLIP_Manakov2022_100_CNN_predictions.tsv
 
-echo "Downloading AGO2_eCLIP_Manakov2022_10_CNN_predictions.tsv"
-gdown "https://drive.google.com/uc?id=$FILE_ID_2" -O predictions/AGO2_eCLIP_Manakov2022_10_CNN_predictions.tsv
-
-echo "Downloading AGO2_eCLIP_Manakov2022_1_CNN_predictions.tsv"
-gdown "https://drive.google.com/uc?id=$FILE_ID_3" -O predictions/AGO2_eCLIP_Manakov2022_1_CNN_predictions.tsv
-
-echo "Download completed! .tsv files are saved in the predictions/ directory."
+echo "Download completed! .tsv files are saved in the $download_dir directory."
