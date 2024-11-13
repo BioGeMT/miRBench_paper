1af3ad6530c3c7dc4843461d493406ee511d3921
David Cechak <davidcechak@gmail.com>
2024-11-13 19:43:35 +0100

    Clean download_predictions script

--- code/plots/Fig5/download_predictions.sh
+++ code/plots/Fig5/download_predictions.sh
@@ -8,10 +8,11 @@ then
 fi
 
 download_dir="predictions"
+plot_output_dir="output"
 FILE_ID_1="15gQ4WShxUZjEjSwppc9cukfusanpbl6n"
 
-# Create a directory to store the downloaded .tsv files
 mkdir -p $download_dir
+mkdir -p $plot_output_dir
 
 echo "Downloading AGO2_eCLIP_Manakov2022_100_CNN_predictions.tsv"
 gdown "https://drive.google.com/uc?id=$FILE_ID_1" -O $download_dir/AGO2_eCLIP_Manakov2022_100_CNN_predictions.tsv
