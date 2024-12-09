99f430e5da0847da3f1caaa0a80275aca86daf8b
Dimos Tzimotoudis <158493651+dimostzim@users.noreply.github.com>
2024-12-09 17:40:36 +0100

    Update map_gene_clusters.py

--- code/clustering/map_gene_clusters.py
+++ code/clustering/map_gene_clusters.py
@@ -1,33 +1,14 @@
 import argparse
 import pandas as pd
 
-def parse_fasta(fasta_path):
-
-    seq_to_id = {}
-    with open(fasta_path) as f:
-        for line in f:
-            line = line.strip()
-            if line.startswith('>'):
-                current_id = line[1:]
-            else:
-                seq_to_id[line] = current_id
-    return seq_to_id
-
 def load_clusters(cluster_csv):
-    
     clusters_df = pd.read_csv(cluster_csv)
     return dict(zip(clusters_df["Seq_ID"], clusters_df["Cluster_ID"]))
 
-def main(cluster_csv, dataset_tsv, output_tsv, fasta_file):
-  
+def main(cluster_csv, dataset_tsv, output_tsv):
     gene_to_cluster = load_clusters(cluster_csv)
-    seq_to_id = parse_fasta(fasta_file)
-
     gene_df = pd.read_csv(dataset_tsv, sep="\t")
-    gene_df["gene_cluster_ID"] = gene_df["gene"].map(
-        lambda seq: gene_to_cluster.get(seq_to_id.get(seq))
-    )
-
+    gene_df["gene_cluster_ID"] = gene_to_cluster.values()
     gene_df.to_csv(output_tsv, sep="\t", index=False)
     print(f"Results saved to {output_tsv}")
 
@@ -36,8 +17,6 @@ if __name__ == "__main__":
     parser.add_argument("--cluster_csv", required=True, help="CSV file containing cluster information")
     parser.add_argument("--dataset_tsv", required=True, help="Input dataset TSV file")
     parser.add_argument("--output_tsv", required=True, help="Output TSV file path")
-    parser.add_argument("--genes_fasta", required=True, help="Input FASTA file with Seq_IDs")
-
     args = parser.parse_args()
-    main(args.cluster_csv, args.dataset_tsv, args.output_tsv, args.genes_fasta)
+    main(args.cluster_csv, args.dataset_tsv, args.output_tsv)
 
