0ac2d9257610f0da9b89fbcc1b665910a1150a32
Stephanie <stephanie.sammut@um.edu.mt>
2025-01-10 08:33:13 +0000

    using get_dataset_df and ommitting header logic

--- code/benchmark_all/benchmark_all.py
+++ code/benchmark_all/benchmark_all.py
@@ -1,6 +1,6 @@
 from miRBench.encoder import get_encoder
 from miRBench.predictor import get_predictor, list_predictors
-from miRBench.dataset import get_dataset_path, list_datasets
+from miRBench.dataset import get_dataset_df, list_datasets
 import pandas as pd
 import argparse
 import os
@@ -27,21 +27,17 @@ def main():
     default_split = "test"
     special_split = "leftout"
 
-    # loop over all available datasets
-    for dset in list_datasets():
-        split = special_split if dset == special_dataset else default_split
-        input_file = get_dataset_path(dset, split=split)
-        output_file = os.path.join(args.out_dir, f"{dset}_{split}_predictions.tsv")
-        header_written = False
-        df = pd.read_csv(input_file, sep='\t')
-        df_preds = benchmark_all(df, dset, split)
-        if not header_written:
-            df_preds.to_csv(output_file, sep='\t', index=False, mode='w')
-            header_written = True
-        else:
-            df_preds.to_csv(output_file, sep='\t', index=False, mode='a', header=False)
+    list_ = ["AGO2_eCLIP_Klimentova2022"]
 
-        print(f"Predictions for {dset} dataset, {splt} split, written to {output_file}")
+    # loop over all available datasets
+    for dset in list_: #list_datasets():
+        split = special_split if dset == special_dataset else default_split
+        df = get_dataset_df(dset, split=split)
+        output_file = os.path.join(args.out_dir, f"{dset}_{split}_predictions.tsv")
+        df_preds = benchmark_all(df, dset, split)
+        df_preds.to_csv(output_file, sep='\t', index=False, mode='w')
+
+        print(f"Predictions for {dset} dataset, {split} split, written to {output_file}")
 
     print("Predictions for all datasets written to the output directory")
 
